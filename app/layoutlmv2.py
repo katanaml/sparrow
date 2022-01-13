23e80e74e3615673071784ceaf977fb71a264ed1
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2022-01-13 20:01:39 +0200

    Response handling

--- app/layoutlmv2.py
+++ app/layoutlmv2.py
@@ -11,12 +11,30 @@ dataset = load_dataset("nielsr/funsd", split="test")
 # define id2label, label2color
 labels = dataset.features['ner_tags'].feature.names
 id2label = {v: k for v, k in enumerate(labels)}
-label2color = {'question':'blue', 'answer':'green', 'header':'orange', 'other':'violet'}
+label2color = {'question': 'blue', 'answer': 'green', 'header': 'orange', 'other': 'violet'}
+
+
+def unnormalize_box(bbox, width, height):
+    return [
+        width * (bbox[0] / 1000),
+        height * (bbox[1] / 1000),
+        width * (bbox[2] / 1000),
+        height * (bbox[3] / 1000),
+    ]
+
+
+def iob_to_label(label):
+    label = label[2:]
+    if not label:
+        return 'other'
+    return label
 
 
 def process_document(image):
     a = datetime.datetime.now()
     print('LayoutLMv2 started')
+
+    width, height = image.size
 
     # encode
     encoding = processor(image, truncation=True, return_offsets_mapping=True, return_tensors="pt")
@@ -29,6 +47,21 @@ def process_document(image):
     predictions = outputs.logits.argmax(-1).squeeze().tolist()
     token_boxes = encoding.bbox.squeeze().tolist()
 
+    # only keep non-subword predictions
+    is_subword = np.array(offset_mapping.squeeze().tolist())[:, 0] != 0
+    true_predictions = [id2label[pred] for idx, pred in enumerate(predictions) if not is_subword[idx]]
+    true_boxes = [unnormalize_box(box, width, height) for idx, box in enumerate(token_boxes) if not is_subword[idx]]
+
+    # draw predictions over the image
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.load_default()
+    for prediction, box in zip(true_predictions, true_boxes):
+        predicted_label = iob_to_label(prediction).lower()
+        draw.rectangle(box, outline=label2color[predicted_label])
+        draw.text((box[0] + 10, box[1] - 10), text=predicted_label, fill=label2color[predicted_label], font=font)
+
     print('LayoutLMv2 completed')
     b = datetime.datetime.now()
     print(b - a)
+
+    return image
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from PIL import Image, ImageDraw, ImageFont
 
 def main():
     image = Image.open('docs/invoice.jpg')
-    process_document(image)
-
+    image = process_document(image)
+    image.save('docs/invoice_processed.jpg')
 
 if __name__ == "__main__":
     main()
