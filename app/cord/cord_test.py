e36af118ce1eab7aa2b07745a4308001e35f83e2
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2022-02-22 15:50:50 +0200

    Removed logging flag

--- app/cord/cord_test.py
+++ app/cord/cord_test.py
@@ -1,6 +1,4 @@
 from datasets import load_dataset
-import datasets
 
-datasets.logging.set_verbosity_info()
 # dataset = load_dataset('/Users/andrejb/infra/shared/katana-git/sparrow/app/cord/cord.py')
 dataset = load_dataset('katanaml/cord')
