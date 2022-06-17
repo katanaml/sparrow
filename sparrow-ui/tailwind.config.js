8b54815d7dc85161cb9a083cb46a71014fd555e0
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2022-06-17 11:36:15 +0300

    UI template setup

--- sparrow-ui/tailwind.config.js
+++ sparrow-ui/tailwind.config.js
@@ -5,7 +5,34 @@ module.exports = {
         './node_modules/flowbite/**/*.js'
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    "50": "#eff6ff",
+                    "100": "#dbeafe",
+                    "200": "#bfdbfe",
+                    "300": "#93c5fd",
+                    "400": "#60a5fa",
+                    "500": "#3b82f6",
+                    "600": "#2563eb",
+                    "700": "#1d4ed8",
+                    "800": "#1e40af",
+                    "900": "#1e3a8a"
+                }
+            },
+            fontFamily: {
+                'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+                'body': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+                'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace']
+            },
+            transitionProperty: {
+                'width': 'width'
+            },
+            textDecoration: ['active'],
+            minWidth: {
+                'kanban': '28rem'
+            },
+        },
     },
     plugins: [
         require('flowbite/plugin')
