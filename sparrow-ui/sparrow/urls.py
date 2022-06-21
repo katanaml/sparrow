6c9cbc743bf138aa0cec23dceb6f1f5608d30d13
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2022-06-21 11:32:47 +0300

    Menu navigation setup

--- sparrow-ui/sparrow/urls.py
+++ sparrow-ui/sparrow/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import dashboard, forms
+from .views import dashboard, forms, tables, identity, ocr, mapping, review, export
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('forms', forms, name='forms')
+    path('forms', forms, name='forms'),
+    path('tables', tables, name='tables'),
+    path('identity', identity, name='identity'),
+    path('ocr', ocr, name='ocr'),
+    path('mapping', mapping, name='mapping'),
+    path('review', review, name='review'),
+    path('export', export, name='export')
 ]
--- sparrow-ui/sparrow/views.py
+++ sparrow-ui/sparrow/views.py
@@ -7,3 +7,27 @@ def dashboard(request):
 
 def forms(request):
     return render(request, 'sparrow/forms.html')
+
+
+def tables(request):
+    return render(request, 'sparrow/tables.html')
+
+
+def identity(request):
+    return render(request, 'sparrow/identity.html')
+
+
+def ocr(request):
+    return render(request, 'sparrow/ocr.html')
+
+
+def mapping(request):
+    return render(request, 'sparrow/mapping.html')
+
+
+def review(request):
+    return render(request, 'sparrow/review.html')
+
+
+def export(request):
+    return render(request, 'sparrow/export.html')
