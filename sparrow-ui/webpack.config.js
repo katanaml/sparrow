c98f894b87ccf51d706556f11ff08b831c8beb1b
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2022-06-22 22:41:43 +0300

    Mobile UI fix

--- sparrow-ui/webpack.config.js
+++ sparrow-ui/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './sparrow/static/scripts/app.js',  // path to our input file
+  entry: './sparrow/static/scripts/app.js',  // path to input file
   output: {
     filename: 'app.bundle.js',  // output bundle file name
     path: path.resolve(__dirname, './sparrow/static/scripts'),
