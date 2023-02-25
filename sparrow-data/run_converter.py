768a1a561541a947aec1a3a526cd4e89a1243f57
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2023-02-25 22:16:24 +0200

    Data files refactoring

--- sparrow-data/run_converter.py
+++ sparrow-data/run_converter.py
@@ -4,7 +4,8 @@ from tools.data_converter import DataConverter
 def main():
     # Convert to sparrow format
     data_converter = DataConverter()
-    data_converter.convert_to_sparrow_format('docs/invoices/ocr', 'docs/invoices/output')
+    data_converter.convert_to_sparrow_format('docs/input/invoices/processed/ocr',
+                                             'docs/input/invoices/processed/output')
 
 if __name__ == '__main__':
     main()
--- sparrow-data/run_ocr.py
+++ sparrow-data/run_ocr.py
@@ -5,11 +5,12 @@ from tools.ocr_extractor import OCRExtractor
 def main():
     # Convert pdf to jpg
     pdf_converter = PDFConverter()
-    pdf_converter.convert_to_jpg('docs/invoices/Dataset with valid information', 'docs/invoices/images')
+    pdf_converter.convert_to_jpg('docs/input/invoices/Dataset with valid information',
+                                 'docs/input/invoices/processed/images')
 
     # OCR
     ocr_extractor = OCRExtractor('db_resnet50', 'crnn_vgg16_bn', pretrained=True)
-    ocr_extractor.extract('docs/invoices', show_prediction=False)
+    ocr_extractor.extract('docs/input/invoices/processed', show_prediction=False)
 
 if __name__ == '__main__':
     main()
