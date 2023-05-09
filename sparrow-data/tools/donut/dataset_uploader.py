128b9dafb7f1d37e9b4b39cd18924c6dea697b61
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2023-05-09 11:05:30 +0300

    Dataset name param

--- sparrow-data/tools/donut/dataset_uploader.py
+++ sparrow-data/tools/donut/dataset_uploader.py
@@ -2,12 +2,12 @@ from pathlib import Path
 from datasets import load_dataset
 
 class DonutDatasetUploader:
-    def upload(self, data_dir):
+    def upload(self, data_dir, dataset_name):
         # define paths
         img_dir_path = Path(data_dir).joinpath("img")
 
         dataset = load_dataset("imagefolder", data_dir=img_dir_path)
 
         # Save dataset: https://huggingface.co/docs/datasets/main/en/image_dataset
-        dataset.push_to_hub("katanaml-org/invoices-donut-data-v1", private=True)
+        dataset.push_to_hub(dataset_name, private=True)
 
