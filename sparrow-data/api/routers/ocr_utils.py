11630af2fd0102ac6b49ced6cab170ab56dda6d8
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2023-06-08 11:03:34 +0300

    Docker deployment support

--- sparrow-data/api/routers/ocr_utils.py
+++ sparrow-data/api/routers/ocr_utils.py
@@ -2,6 +2,6 @@ def merge_data(values):
     data = []
     for idx in range(len(values)):
         data.append([values[idx][1][0]])
-        print(data[idx])
+        # print(data[idx])
 
     return data
