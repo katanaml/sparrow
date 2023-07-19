dc6fee540b93e7a03a88be38c3f26d52b689aefe
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2023-07-19 11:37:22 +0300

    Port update

--- sparrow-data/api/start.sh
+++ sparrow-data/api/start.sh
@@ -6,5 +6,6 @@ if [[ $RUN_LOCALLY = "true" ]]; then
     source ./set_env_vars.sh
 fi
 
-# Then start FastAPI application, set to 0.0.0.0 to allow external connections from Docker container
+# Then start FastAPI application, set to 0.0.0.0 to allow external connections from Docker container.
+# Remove --reload for production.
 uvicorn endpoints:app --host 127.0.0.1 --port 8000 --reload
