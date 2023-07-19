196a5cf0cd249d44a55e07d76dce4458c6622d7e
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2023-07-19 11:57:00 +0300

    Update set_env_vars.sh

--- sparrow-plugin-ui/set_env_vars.sh
+++ sparrow-plugin-ui/set_env_vars.sh
@@ -6,3 +6,4 @@ export API_DATA_URL="http://127.0.0.1:8000/api-ocr/v1/sparrow-data/ocr"
 
 # Print environment variables to verify they're set
 echo "SPARROW_KEY: $SPARROW_KEY"
+echo "API_DATA_URL: $API_DATA_URL"
