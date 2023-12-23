83b63f15d9c1b7dde2974d9f83fea1dd81cb4200
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2023-12-23 22:26:03 +0200

    Update pipeline.py

--- sparrow-ml/lemming/rag/pipeline.py
+++ sparrow-ml/lemming/rag/pipeline.py
@@ -4,38 +4,51 @@ from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from llama_index.llms import Ollama
 from llama_index.vector_stores import WeaviateVectorStore
 import weaviate
-from pydantic import create_model
+from pydantic import create_model, BaseModel
 from typing import List
 import box
 import yaml
 
 
-# class InvoiceInfo(BaseModel):
-#     invoice_number: int
-#     # invoice_date: str
-#     # client_name: str
-#     # client_address: str
-#     # client_tax_id: str
-#     # seller_name: str
-#     # seller_address: str
-#     # seller_tax_id: str
-#     # iban: str
-#     # names_of_invoice_items: List[str]
-#     # gross_worth_of_invoice_items: List[float]
-#     # total_gross_worth: str
+class InvoiceInfo(BaseModel):
+    # invoice_number: int
+    # invoice_date: str
+    # client_name: str
+    # client_address: str
+    # client_tax_id: str
+    # seller_name: str
+    # seller_address: str
+    # seller_tax_id: str
+    # iban: str
+    # names_of_invoice_items: List[str]
+    gross_worth_of_invoice_items: List[float]
+    # total_gross_worth: str
 
-def build_response_class(query_inputs, query_types):
-    type_mapping = {
-        'int': int,
+
+# Function to safely evaluate type strings
+def safe_eval_type(type_str, context):
+    try:
+        return eval(type_str, {}, context)
+    except NameError:
+        raise ValueError(f"Type '{type_str}' is not recognized")
+
+
+def build_response_class(query_inputs, query_types_as_strings):
+    # Controlled context for eval
+    context = {
+        'List': List,
         'str': str,
+        'int': int,
         'float': float
+        # Include other necessary types or typing constructs here
     }
 
-    query_types = [type_mapping[type_str] for type_str in query_types]
+    # Convert string representations to actual types
+    query_types = [safe_eval_type(type_str, context) for type_str in query_types_as_strings]
 
+    # Create fields dictionary
     fields = {name: (type_, ...) for name, type_ in zip(query_inputs, query_types)}
 
-    # Create a dynamic model
     DynamicModel = create_model('DynamicModel', **fields)
 
     return DynamicModel
@@ -81,14 +94,14 @@ def build_rag_pipeline(query_inputs, query_types, debug=False):
     print("Building index...")
     index = build_index(cfg.CHUNK_SIZE, llm, embeddings, client, cfg.INDEX_NAME)
 
-    print("Building response class...")
+    print("Building dynamic response class...")
     DynamicModel = build_response_class(query_inputs, query_types)
 
     print("Constructing query engine...")
 
     query_engine = index.as_query_engine(
         streaming=False,
-        output_cls=DynamicModel,
+        output_cls=InvoiceInfo,
         response_mode="compact"
     )
 
