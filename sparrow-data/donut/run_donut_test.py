bf0d59bf589871584319f6c27e1b097f11e32d81
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2024-01-30 17:05:14 +0200

    refactoring

--- sparrow-data/donut/run_donut_test.py
+++
@@ -1,8 +0,0 @@
-from tools.donut.dataset_tester import DonutDatasetTester
-
-def main():
-    dataset_tester = DonutDatasetTester()
-    dataset_tester.test("katanaml-org/invoices-donut-data-v1")
-
-if __name__ == '__main__':
-    main()
--- sparrow-data/donut/run_donut_upload.py
+++
@@ -1,8 +0,0 @@
-from tools.donut.dataset_uploader import DonutDatasetUploader
-
-def main():
-    dataset_uploader = DonutDatasetUploader()
-    dataset_uploader.upload('docs/models/donut/data', "katanaml-org/invoices-donut-data-v1")
-
-if __name__ == '__main__':
-    main()
