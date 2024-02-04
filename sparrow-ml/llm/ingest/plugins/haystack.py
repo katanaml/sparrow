21983a934409cb496e6dcf1ee0114f308c30eaa6
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2024-02-04 21:30:38 +0200

    Haystack ingest

--- sparrow-ml/llm/ingest/plugins/haystack.py
+++ sparrow-ml/llm/ingest/plugins/haystack.py
@@ -1,11 +1,10 @@
 from ingest.plugins.interface import Ingest
 from haystack.components.converters import PyPDFToDocument
 from haystack.components.routers import FileTypeRouter
-from haystack.components.joiners import DocumentJoiner
 from haystack.components.preprocessors import DocumentSplitter, DocumentCleaner
 from haystack.components.embedders import SentenceTransformersDocumentEmbedder
 from haystack.pipeline import Pipeline
-from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack_integrations.document_stores.chroma import ChromaDocumentStore
 from haystack.components.writers import DocumentWriter
 import timeit
 import os
@@ -29,10 +28,9 @@ class HaystackIngest(Ingest):
 
         start = timeit.default_timer()
 
-        document_store = InMemoryDocumentStore()
+        document_store = ChromaDocumentStore()
         file_type_router = FileTypeRouter(mime_types=["application/pdf"])
         pdf_converter = PyPDFToDocument()
-        document_joiner = DocumentJoiner()
 
         document_cleaner = DocumentCleaner()
         document_splitter = DocumentSplitter(
@@ -47,15 +45,13 @@ class HaystackIngest(Ingest):
         preprocessing_pipeline = Pipeline()
         preprocessing_pipeline.add_component(instance=file_type_router, name="file_type_router")
         preprocessing_pipeline.add_component(instance=pdf_converter, name="pypdf_converter")
-        preprocessing_pipeline.add_component(instance=document_joiner, name="document_joiner")
         preprocessing_pipeline.add_component(instance=document_cleaner, name="document_cleaner")
         preprocessing_pipeline.add_component(instance=document_splitter, name="document_splitter")
         preprocessing_pipeline.add_component(instance=document_embedder, name="document_embedder")
         preprocessing_pipeline.add_component(instance=document_writer, name="document_writer")
 
         preprocessing_pipeline.connect("file_type_router.application/pdf", "pypdf_converter.sources")
-        preprocessing_pipeline.connect("pypdf_converter", "document_joiner")
-        preprocessing_pipeline.connect("document_joiner", "document_cleaner")
+        preprocessing_pipeline.connect("pypdf_converter", "document_cleaner")
         preprocessing_pipeline.connect("document_cleaner", "document_splitter")
         preprocessing_pipeline.connect("document_splitter", "document_embedder")
         preprocessing_pipeline.connect("document_embedder", "document_writer")
@@ -65,6 +61,9 @@ class HaystackIngest(Ingest):
         preprocessing_pipeline.run({
             "file_type_router": {"sources": file_list}
         })
+
+        print(f"Number of documents in document store: {document_store.count_documents()}")
+        print("Haystack ingest is not used, you should run inference directly. Ingest will execute as a part of the inference.")
 
         end = timeit.default_timer()
         print(f"Time to prepare embeddings: {end - start}")
