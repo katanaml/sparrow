164aa4d6a12f1f34ca4c421598303fcb72bb1b9b
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2024-02-26 15:37:54 +0200

    LlamaIndex Multimodal test

--- sparrow-ml/llm/rag/agents/vllamaindex.py
+++ sparrow-ml/llm/rag/agents/vllamaindex.py
@@ -4,8 +4,13 @@ import warnings
 import os
 import box
 import yaml
+import timeit
 from rich import print
-from ollama import Client
+from pathlib import Path
+from llama_index.core import SimpleDirectoryReader
+from PIL import Image
+import matplotlib.pyplot as plt
+from llama_index.multi_modal_llms.ollama import OllamaMultiModal
 
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -27,23 +32,26 @@ class VLlamaIndexPipeline(Pipeline):
                      local: bool = True) -> Any:
         print(f"\nRunning pipeline with {payload}\n")
 
+        start = timeit.default_timer()
+
         file_list = [os.path.join(cfg.DATA_PATH, f) for f in os.listdir(cfg.DATA_PATH)
                      if os.path.isfile(os.path.join(cfg.DATA_PATH, f)) and not f.startswith('.')
                      and not f.lower().endswith('.pdf')]
 
-        query = "retrieve values of invoice number, invoice date fields from given document. im interested in actual values, no need to provide any descriptions"
         print("Query: " + query + "\n")
 
-        client = Client(host='http://127.0.0.1:11434')
-        res = client.chat(
-            model="llava:13b",
-            messages=[
-                {
-                    'role': 'user',
-                    'content': query,
-                    'images': file_list
-                }
-            ]
-        )
+        mm_model = OllamaMultiModal(model="llava:13b")
 
-        print(res['message']['content'])
+        # load as image documents
+        image_documents = SimpleDirectoryReader(cfg.DATA_PATH + "temp/").load_data()
+
+
+        end = timeit.default_timer()
+
+        print(f"\nJSON response:\n")
+        print("answer" + '\n')
+        print('=' * 50)
+
+        print(f"Time to retrieve answer: {end - start}\n")
+
+
