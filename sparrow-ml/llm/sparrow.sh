8025b12ac04975607adab97ecba59d8b0a33b8ea
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2024-03-22 13:42:27 +0200

    Environment setup

--- sparrow-ml/llm/sparrow.sh
+++ sparrow-ml/llm/sparrow.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-command -v python3 >/dev/null 2>&1 || { echo >&2 "Python 3 is required but it's not installed. Aborting."; exit 1; }
+command -v python >/dev/null 2>&1 || { echo >&2 "Python is required but it's not installed. Aborting."; exit 1; }
+
+# Check Python version
+PYTHON_VERSION=$(python --version 2>&1) # Capture both stdout and stderr
+echo "Detected Python version: $PYTHON_VERSION"
+if [[ ! "$PYTHON_VERSION" == *"3.10.4"* ]]; then
+  echo "Python version 3.10.4 is required. Current version is $PYTHON_VERSION. Aborting."
+  exit 1
+fi
 
 PYTHON_SCRIPT_PATH="engine.py"
 
@@ -15,6 +23,6 @@ if [ "$1" == "assistant" ]; then
     shift # Shift the arguments to exclude the first one
 fi
 
-python3 "${PYTHON_SCRIPT_PATH}" "$@"
+python "${PYTHON_SCRIPT_PATH}" "$@"
 
 # make script executable with: chmod +x sparrow.sh
