c2e5ea5a28c0e2e90b9005644a64f029a095ecb5
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2024-09-25 21:48:33 +0300

    VL LLM agent integration

--- sparrow-ml/llm/rag/agents/sparrow_parse/sparrow_parse.py
+++ sparrow-ml/llm/rag/agents/sparrow_parse/sparrow_parse.py
@@ -1,10 +1,13 @@
 from rag.agents.interface import Pipeline
+from sparrow_parse.vllm.inference_factory import InferenceFactory
+from sparrow_parse.extractors.vllm_extractor import VLLMExtractor
 import timeit
 import box
 import yaml
 from rich import print
 from typing import Any, List
 import warnings
+import os
 
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -33,15 +36,42 @@ class SparrowParsePipeline(Pipeline):
 
         start = timeit.default_timer()
 
-        answer = "OK"
+        query_all_data = False
+        query = None
+        if query_inputs[0] == "*" and query_types[0] == "*":
+            query_all_data = True
+            query = None
+
+        extractor = VLLMExtractor()
+
+        # export HF_TOKEN="hf_"
+        config = {
+            "method": "huggingface",  # Could be 'huggingface' or 'local_gpu'
+            "hf_space": "katanaml/sparrow-qwen2-vl-7b",
+            "hf_token": os.getenv('HF_TOKEN')
+        }
+
+        # Use the factory to get the correct instance
+        factory = InferenceFactory(config)
+        model_inference_instance = factory.get_inference_instance()
+
+        input_data = [
+            {
+                "image": file_path,
+                "text_input": query
+            }
+        ]
+
+        # Now you can run inference without knowing which implementation is used
+        result = extractor.run_inference(model_inference_instance, input_data, generic_query=query_all_data, debug=True)
 
         end = timeit.default_timer()
 
         print(f"\nJSON response:\n")
-        print(answer)
+        print(result)
         print('\n')
         print('=' * 50)
 
         print(f"Time to retrieve answer: {end - start}")
 
-        return answer
+        return result
