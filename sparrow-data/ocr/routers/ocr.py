c746fbb422096323b5068137f52d85244ae24902
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2025-10-12 16:26:14 +0300

    Update ocr.py

--- sparrow-data/ocr/routers/ocr.py
+++ sparrow-data/ocr/routers/ocr.py
@@ -30,8 +30,8 @@ router = APIRouter()
 @lru_cache(maxsize=1)
 def load_ocr_model():
     ocr = PaddleOCR(
-        text_detection_model_name="PP-OCRv5_mobile_det",
-        text_recognition_model_name="PP-OCRv5_mobile_rec",
+        text_detection_model_name="PP-OCRv5_server_det",
+        text_recognition_model_name="PP-OCRv5_server_rec",
         use_doc_orientation_classify=False,
         use_doc_unwarping=False,
         use_textline_orientation=False)
