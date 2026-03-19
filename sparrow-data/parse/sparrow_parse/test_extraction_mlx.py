629be137d8c681968d4498c501e227ee33df4a08
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2026-03-19 10:23:28 +0200

    Update test_extraction_mlx.py

--- sparrow-data/parse/sparrow_parse/test_extraction_mlx.py
+++ sparrow-data/parse/sparrow_parse/test_extraction_mlx.py
@@ -6,7 +6,6 @@ import time
 
 # Load model and processor
 # model_path = "lmstudio-community/Mistral-Small-3.2-24B-Instruct-2506-MLX-8bit"
-# model_path = "mlx-community/Ministral-3-14B-Instruct-2512-bf16"
 # model_path = "mlx-community/Ministral-3-14B-Instruct-2512-8bit"
 # model_path = "mlx-community/dots.ocr-bf16"
 # model_path = "mlx-community/DeepSeek-OCR-2-bf16"
